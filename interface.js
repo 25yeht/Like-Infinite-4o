3dde8920d53bd449293b3be2d3a3309bf0bdab02
25yeht <76498360+25yeht@users.noreply.github.com>
2024-05-30 13:01:11 -0500

    Update interface.js

--- interface.js
+++ interface.js
@@ -1,4 +1,10 @@
-var inputBox = document.querySelector("#prompt-textarea");
+var inputBox;
+var sendBtn;
+
+function updateEls() {
+    inputBox = document.querySelector("#prompt-textarea");
+    sendBtn = document.querySelector("[data-testid=send-button]");
+}
 
 async function wait(ms) {
     await new Promise(function(r) {
@@ -18,4 +24,15 @@ async function waitUntilInput() {
     };
 
     await promise;
+}
+
+async function sendGPT(message) {
+    updateEls();
+
+    inputBox.value = message;
+    await waitUntilInput();
+    await wait(100);
+    sendBtn.click();
+
+    await waitUntilDone();
 }
